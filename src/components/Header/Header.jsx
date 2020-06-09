81cbc0b209aa120c3856807516676971c55bcd92
Gustavo <gcrodrigues413@gmail.com>
2020-06-08 23:41:31 -0300

    Header component development

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
-import { FaGithub, FaSearch } from 'react-icons/fa'
-import styles from './Header.module.css';
+import React, { useEffect, useContext } from "react";
+import { FaGithub } from "react-icons/fa";
+import { Context } from "../../hooks/UserInfosContext";
+import { Form } from "../";
 
-
+import styles from "./Header.module.css";
 
 const Header = () => {
-  return(
-    <div className={styles.container}>
-      <h1 className={styles.title}><FaGithub /> Repositories</h1>
-      
-      <form className={styles.searchForm}>
-        <input type="text"/>
-        <button><FaSearch /></button>
-      </form>
+  const { sizeValue } = useContext(Context);
+  const [size, setSize] = sizeValue;
 
-      
+  useEffect(() => {
+    function windowResize() {
+      setSize(window.innerWidth);
+    }
+    window.addEventListener("resize", windowResize);
+  });
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <FaGithub style={{ marginRight: 10 }} /> Repositories
+      </h1>
+
+      {size <= 770 ? "" : <Form />}
     </div>
   );
-}
+};
 
 export default Header;
