065e7f3fd696511ed4f4c6732513f9c1c2f10b2e
Gustavo <gcrodrigues413@gmail.com>
2020-06-09 00:04:08 -0300

    Components export file

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
-export { default as Header } from './Header/Header';
-export { default as Repository } from './Repository/Repository';
+export { default as Header } from "./Header/Header";
+export { default as User } from "./User/User";
+export { default as Repository } from "./Repository/Repository";
+export { default as RepositoryList } from "./RepositoryList/RepositoryList";
+export { default as Form } from "./Form/Form";
